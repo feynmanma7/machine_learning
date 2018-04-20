54db3db17fd7bf39b7c0b0f9904dd1044ebb22bc
wadefall <flyingman26@sina.com>
2018-04-20 16:12:54 +0800

    add batch_size
    still to be checked, scikit-learn is using Least Squares method

--- linear_regression.py
+++ linear_regression.py
@@ -4,33 +4,42 @@ import numpy as np
 import sys
 np.random.seed(20170430)
 
+
 class LinearRegression():
 
-	alpha = 1e-4
-	max_iter = 100
-	threshold = 1e-4
-	coef_ = []
-	intercept_ = 0
-
 	def __init__(self, 
-		alpha=1e-4, 
+		alpha=0.01, 
 		max_iter=100, 
-		threshold=1e-6, 
-		dim=2):
+		threshold=1e-4, 
+		dim=2,
+		batch_size=10):
 		self.alpha = alpha
 		self.max_iter = max_iter
 		self.threshold = threshold
 		self.coef_ = np.random.random(dim)
 		self.intercept_ = np.random.random()
-		#self.intercept = 0.
+		self.batch_size = batch_size
 
 
 	def _compute_loss(self, X, y):
+
+		loss = 0
+
+		for i in range(len(X)):
+			h = np.dot(X[i], self.coef_) + self.intercept_
+
+			loss += (h - y[i]) ** 2
+
+		return loss 
+
+		'''
 		return np.sum(list(map(
-			lambda a, b: (np.sum(self.coef_ * a) + self.intercept_ - b) ** 2, X, y)))
+			lambda a, b: (np.dot(self.coef_, a) + self.intercept_ - b) ** 2, X, y)))
+		'''
 
 
 	def fit(self, X, y):
+
 		'''
 		f = W * X + b
 		loss = sum_{i=1}^{N} (f(i) - y[i]) ** 2
@@ -46,28 +55,41 @@ class LinearRegression():
 
 		pre_loss = 0
 
+		num = 0
+		delta_b = 0
+		delta_w = np.zeros(self.coef_.size)
+
 		for iter in range(self.max_iter):
 
 			for i in range(len(X)):
 				
-				f = np.sum(self.coef_ * X[i]) + self.intercept_
+				h = np.dot(self.coef_, X[i]) + self.intercept_
 
-				delta_b = 2. * (f - y[i])
+				if num < self.batch_size:
+					num += 1
+					delta_b += 2. * (h - y[i])
 
-				for j in range(self.coef_.size):
-					delta_w = delta_b * X[i][j]
-					self.coef_[j] = self.coef_[j] - self.alpha * delta_w
-				
-				self.intercept_ = self.intercept_ - self.alpha * delta_b
+					for j in range(self.coef_.size):
+						delta_w[j] += 2. * (h - y[i]) * X[i][j]
 
-			cur_loss = self._compute_loss(X, y)
+				else:
 
-			pre_loss = cur_loss
+					for j in range(self.coef_.size):
+						self.coef_[j] -= self.alpha * delta_w[j] / self.batch_size
+						delta_w[j] = 0
+					self.intercept_ -=  self.alpha * delta_b / self.batch_size
+					delta_b = 0
 
-			delta_loss = cur_loss - pre_loss
+					num = 0
 
-			if delta_loss < self.threshold:
-				return
+
+					cur_loss = self._compute_loss(X, y)
+					delta_loss = cur_loss - pre_loss
+
+					pre_loss = cur_loss
+
+					if delta_loss < self.threshold:
+						return
 
 
 	def predict(self, X):
@@ -76,8 +98,8 @@ class LinearRegression():
 
 def main():
 	reg = LinearRegression()
-	X = [[0., 0.], [1., 1.], [2., 2.], [3., 3.], [4, 4], [5, 5], [6, 6]]
-	y = [0, 1, 2, 3, 4, 5, 6]
+	X = [[0., 0.], [1., 1.], [2., 2.]]
+	y = [0, 1, 2]
 	reg.fit(X, y)
 	print(reg.coef_, reg.intercept_)
 	print(reg._compute_loss(X, y))
