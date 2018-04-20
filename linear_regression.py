eecbe31d3c777e279ccdc629ed8af051ee5ba20d
wadefall <flyingman26@sina.com>
2018-04-20 17:38:53 +0800

    fixed
    almost right, tuning of learning rate and n_epoch is important.

--- linear_regression.py
+++ linear_regression.py
@@ -1,108 +1,101 @@
-#encoding:utf-8
+# encoding:utf-8
 from functools import reduce
 import numpy as np
 import sys
+
 np.random.seed(20170430)
+from sklearn import datasets
 
 
 class LinearRegression():
-
-	def __init__(self, 
-		alpha=0.01, 
-		max_iter=100, 
-		threshold=1e-4, 
-		dim=2,
-		batch_size=10):
-		self.alpha = alpha
-		self.max_iter = max_iter
-		self.threshold = threshold
-		self.coef_ = np.random.random(dim)
-		self.intercept_ = np.random.random()
-		self.batch_size = batch_size
+    def __init__(self,
+                 alpha=0.001,
+                 n_epoch=10000,
+                 threshold=1e-4):
+        self.alpha = alpha
+        self.n_epoch = n_epoch
+        self.threshold = threshold
 
 
-	def _compute_loss(self, X, y):
+    def _compute_loss(self, X, y):
+        loss = 0
 
-		loss = 0
+        for i in range(len(X)):
+            pred = self.predict(X[i])
+            error = pred - y[i]
+            loss += error ** 2
 
-		for i in range(len(X)):
-			h = np.dot(X[i], self.coef_) + self.intercept_
-
-			loss += (h - y[i]) ** 2
-
-		return loss 
-
-		'''
-		return np.sum(list(map(
-			lambda a, b: (np.dot(self.coef_, a) + self.intercept_ - b) ** 2, X, y)))
-		'''
+        return loss / len(X)
 
 
-	def fit(self, X, y):
+    def fit(self, X, y):
 
-		'''
-		f = W * X + b
-		loss = sum_{i=1}^{N} (f(i) - y[i]) ** 2
+        '''
+        f = W * X + b
+        loss = sum_{i=1}^{N} (f(i) - y[i]) ** 2
 
-		SGD
-		delta_{w_j} = 2 (f(i) - y[i]) * X[i][j]
-		delta_{b} = 2 (f[i] - y[i])
+        SGD
+        delta_{w_j} = 2 (f(i) - y[i]) * X[i][j]
+        delta_{b} = 2 (f[i] - y[i])
 
-		w_j = w_j - alpha * delta_{w_j}
-		b = b - alpha * delta_{b}
+        w_j = w_j - alpha * delta_{w_j}
+        b = b - alpha * delta_{b}
+        '''
 
-		'''
+        self.coef_ = np.random.random(len(X[0]))
+        self.intercept_ = np.random.random()
 
-		pre_loss = 0
+        pre_loss = 0
 
-		num = 0
-		delta_b = 0
-		delta_w = np.zeros(self.coef_.size)
+        for epoch in range(self.n_epoch):
 
-		for iter in range(self.max_iter):
+            for i in range(len(X)):
+                pred = self.predict(X[i])
 
-			for i in range(len(X)):
-				
-				h = np.dot(self.coef_, X[i]) + self.intercept_
+                error = pred - y[i]
 
-				if num < self.batch_size:
-					num += 1
-					delta_b += 2. * (h - y[i])
+                self.intercept_ -= self.alpha * (2 * error)
 
-					for j in range(self.coef_.size):
-						delta_w[j] += 2. * (h - y[i]) * X[i][j]
+                for j in range(self.coef_.size):
+                    self.coef_[j] -= self.alpha * (2 * error) * X[i][j]
 
-				else:
+            # compute loss
+            cur_loss = self._compute_loss(X, y)
+            delta_loss = abs(pre_loss - cur_loss)
+            pre_loss = cur_loss
 
-					for j in range(self.coef_.size):
-						self.coef_[j] -= self.alpha * delta_w[j] / self.batch_size
-						delta_w[j] = 0
-					self.intercept_ -=  self.alpha * delta_b / self.batch_size
-					delta_b = 0
-
-					num = 0
+            print('epoch%s, loss: %s, coefs: %s, intercepts: %s'
+                  % (epoch, cur_loss, self.coef_, self.intercept_))
 
 
-					cur_loss = self._compute_loss(X, y)
-					delta_loss = cur_loss - pre_loss
-
-					pre_loss = cur_loss
-
-					if delta_loss < self.threshold:
-						return
+            # check covergence
+            if delta_loss < self.threshold:
+                return
 
 
-	def predict(self, X):
-		pass
+
+    def predict(self, x):
+        return np.dot(self.coef_, x) + self.intercept_
+
 
 
 def main():
-	reg = LinearRegression()
-	X = [[0., 0.], [1., 1.], [2., 2.]]
-	y = [0, 1, 2]
-	reg.fit(X, y)
-	print(reg.coef_, reg.intercept_)
-	print(reg._compute_loss(X, y))
+    reg = LinearRegression()
+
+    diabetes = datasets.load_diabetes()
+    diabetes_X = diabetes.data[:, np.newaxis, 2]
+
+    diabetes_X_train = diabetes_X[:-20]
+    diabetes_X_test = diabetes_X[-20:]
+
+    diabetes_y_train = diabetes.target[:-20]
+    diabetes_y_test = diabetes.target[-20:]
+
+    reg.fit(diabetes_X_train, diabetes_y_train)
+
+    #reg.fit(X, y)
+    print(reg.coef_, reg.intercept_)
+
 
 if __name__ == '__main__':
-	main()
+    main()
