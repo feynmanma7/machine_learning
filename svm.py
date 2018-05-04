f938a1de3220d711e9edca5b71ccbd94accfb9b1
wadefall <flyingman26@sina.com>
2018-05-04 15:07:52 +0800

    procedure
    SMO needed.

--- svm.py
+++ svm.py
@@ -8,6 +8,19 @@ class SVMClassifier:
 
 
     def fit(self, X, y):
+        '''
+        Loss function: (1) max soft margin; (2) hinge loss with l2 norm;
+        J = 1/2 * ||w||^2 + C \sum_{i=1}^{N} Epsilon_i
+        s.t. Epsilon_i >= 0, y_i (W^T * X_i + w_0) >= 1 - Epsilon_i
+
+        (1) Lagrange function, L(w, b, e_i, a_i)
+        (2) Dual Problem,  max_{a_i} min_{w, b, e_i} L(w, b, e_i, a_i)
+        Solve dual problem, get optimal a_i
+        (3) Solve KKT conditions, get optimal w, b, e_i.
+
+        Need SMO.
+
+        '''
         pass
 
 
