b75903c9c7f300d628637a32747237134652e058
wadefall <flyingman26@sina.com>
2018-12-27 09:41:16 +0800

    recursive auto-grad
    recursive auto-grad

--- returing/returing/nn/operation/base/__init__.py
+++ returing/returing/nn/operation/base/__init__.py
@@ -9,10 +9,12 @@ class Add(Operation):
     A = None
     B = None
 
-    def __init__(self):
+    def __init__(self, name):
         super(Add, self).__init__()
-        self.A = Tensor()
-        self.B = Tensor()
+        self.A = Tensor
+        self.B = Tensor
+        self.op_name = 'add'
+        self.name = name
 
     def forward(self, *args):
         assert len(args) == 2
@@ -23,30 +25,54 @@ class Add(Operation):
         self.B = args[1]
 
         C = Tensor(self.A.data + self.B.data)
+        C.name = self.name
         C.grad_fn = self
+
+        if self.A.requires_grad or self.B.requires_grad:
+            C.requires_grad = True
+
+        self.A.parent = C
+        self.B.parent = C
+        C.left_child = self.A
+        C.right_child = self.B
+
         return C
 
-    def backward(self):
-        if self.A.is_grad:
-            self.A.grad = 1
+    def backward(self, grad_out=1):
+        if self.A.requires_grad:
+            if not self.A.grad:
+                self.A.grad = 1 * grad_out
+            else:
+                self.A.grad += 1 * grad_out
 
-        if self.B.is_grad:
-            self.B.grad = 1
+        if self.B.requires_grad:
+            if not self.B.grad:
+                self.B.grad = 1 * grad_out
+            else:
+                self.B.grad += 1 * grad_out
 
 
 if __name__ == '__main__':
 
-    a = Tensor(np.array([1, 2, 3, 4]), is_grad=True)
-    b = Tensor(np.array([2, 3, 4, 5]), is_grad=False)
+    a = Tensor(np.array([1, 2, 3, 4]), requires_grad=True, name='a')
+    b = Tensor(np.array([2, 3, 4, 5]), requires_grad=True, name='b')
 
     a.print()
     b.print()
 
-    c = Add()(a, b)
-    d = Add()(c, a)
+    c = Add('c')(a, b)
+    d = Add('d')(a, b)
+    e = Add('e')(d, c)
 
-    d.backward()
+    e.print()
+
+    e.backward()
     print(a.grad)
     print(b.grad)
+    print(c.grad)
+    print(d.grad)
 
-    d.print()
+
+
+
+
--- returing/returing/nn/tensor/__init__.py
+++ returing/returing/nn/tensor/__init__.py
@@ -2,19 +2,41 @@ import numpy as np
 np.random.seed(20170430)
 
 
+class Node(object):
+
+    grad_fn = None
+    tensor = None
+
+    parent = None
+    left_child = None
+    right_child = None
+
+    def __init__(self):
+        super(Node, self).__init__()
+
+
 class Tensor(object):
 
     data = None
-    is_grad = None # has_grad or not
+    name = None
+    #is_leaf = None
+    requires_grad = None
     grad_fn = None
     grad = None
 
+    # Point to Tensor
+    left_child = None
+    right_child = None
+    parent = None
+
     def __init__(self,
                  data=None,
-                 is_grad=False):
+                 requires_grad=False,
+                 name=None):
         super(Tensor, self).__init__()
         self.data = data
-        self.is_grad = is_grad
+        self.requires_grad = requires_grad
+        self.name = name
 
     def set_data(self, data):
         self.data = data
@@ -23,14 +45,53 @@ class Tensor(object):
         self.grad = grad
 
     def backward(self):
-        return self.grad_fn.backward()
+
+        if not self.grad_fn:
+            return
+
+        grad_out = 1
+        if self.grad:
+            grad_out = self.grad
+
+        self.grad_fn.backward(grad_out)
+
+        if self.left_child:
+            self.left_child.backward()
+
+        if self.right_child:
+            self.right_child.backward()
+
+        """
+        if self.left_child and self.left_child.grad_fn:
+            self.left_child.grad_fn.backward()
+        if self.right_child and self.right_child.grad_fn:
+            self.right_child.grad_fn.backward()
+        """
 
     def print(self):
         print(self.data)
 
-        if self.is_grad:
-            print('is_grad =', self.is_grad)
+        if self.requires_grad:
+            print('requires_grad =', self.requires_grad)
         if self.grad_fn:
             print('grad_fn = ', self.grad_fn)
 
         print('\n')
+
+
+    def _print_graph(self, tensor, parent_to_child=True):
+
+        if parent_to_child:
+            if tensor == None:
+                return
+            print("node.data %s" % tensor.data)
+            print("grad_fn %s" % tensor.grad_fn)
+            print()
+
+            self._print_graph(tensor.left_child, parent_to_child)
+            self._print_graph(tensor.right_child, parent_to_child)
+
+    def print_graph(self, parent_to_child=True):
+        if parent_to_child:
+            self._print_graph(self, parent_to_child)
+
