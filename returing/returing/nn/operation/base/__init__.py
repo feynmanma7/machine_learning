Conv2D, fixed bug, but overflow
Conv2D, fixed bug, but overflow

returing/returing/nn/operation/base/__init__.py
@@ -44,12 +44,11 @@ class Add(Operation):
         C.left_child = self.A
         C.right_child = self.B
 
+        self.output_shape = C.data.shape
+
         return C
 
-    def backward(self, C_grad=1):
-
-        # assert self.A.data.shape == self.B.data.shape
-
+    def backward(self, C_grad=None):
         if isinstance(self.A.data, np.ndarray):
             shape = self.A.data.shape
         elif isinstance(self.B.data, np.ndarray):
@@ -57,22 +56,27 @@ class Add(Operation):
         else:
             return
 
-        #grad_mat = np.ones(self.A.data.shape)
-        grad_mat = np.ones(shape)
+        cur_grad = np.ones(shape)
+
+        if isinstance(C_grad, np.ndarray):
+            assert C_grad.shape == self.output_shape
+
+            assert cur_grad.shape == C_grad.shape
+            cur_grad *= cur_grad
 
         if self.A.requires_grad:
             if not isinstance(self.A.grad, np.ndarray):
                 # if isinstance(C_grad, np.ndarray),
                 # '*' is element-wise multiply
-                self.A.grad = grad_mat * C_grad
+                self.A.grad = cur_grad
             else:
-                self.A.grad += grad_mat * C_grad
+                self.A.grad += cur_grad
 
         if self.B.requires_grad:
             if not isinstance(self.B.grad, np.ndarray):
-                self.B.grad = grad_mat * C_grad
+                self.B.grad = cur_grad
             else:
-                self.B.grad += grad_mat * C_grad
+                self.B.grad += cur_grad
 
 
 class Subtract(Operation):
@@ -133,13 +137,14 @@ class Sum(Operation):
     # Unary operation
     A = None
 
-    def __init__(self, axis=None, name=None):
+    def __init__(self, axis=None, target_shape=None, name=None):
         super(Sum, self).__init__()
         self.A = Tensor
         self.op_name = 'sum'
         self.name = name
 
         self.axis = axis
+        self.target_shape = target_shape
 
     def forward(self, *args):
 
@@ -148,7 +153,13 @@ class Sum(Operation):
 
         self.A = args[0]
 
-        C = Tensor(np.sum(self.A.data, axis=self.axis))
+        C_data = np.sum(self.A.data, axis=self.axis)
+        if isinstance(self.target_shape, tuple):
+            C_data = C_data.reshape(self.target_shape)
+
+        self.output_shape = C_data.shape
+
+        C = Tensor(C_data)
 
         C.name = self.name
         C.grad_fn = self
@@ -163,28 +174,30 @@ class Sum(Operation):
 
         return C
 
-    def backward(self, C_grad=1):
+    def backward(self, grad_out=None):
         # Add attribute `axis` to the forward function,
         # for backward, the gradient keeps the same with the input Tensor  `A`.
 
-        if not self.A:
-            return
-
         assert isinstance(self.A.data, np.ndarray)
 
-        cur_grad = np.ones(self.A.data.shape)
-        if isinstance(C_grad, np.ndarray):
-            # C_grad.shape may not be the same with cur_grad.shape
-            # If C_grad is the same shape with Y_pred, which is obtained by np.sum()
-            # Numpy will handle here.
-            cur_grad *= C_grad
+        if isinstance(grad_out, np.ndarray):
+            assert grad_out.shape == self.output_shape
+            cur_grad = grad_out # grad_out * ones
+        else:
+            cur_grad = np.ones(self.output_shape) # ones
+
+        # cur_grad, Reshape to A.shape
+        repeat_time = 1
+        for dim in self.A.data.shape:
+            repeat_time *= dim
+        for dim in self.output_shape:
+            repeat_time /= dim
+        cur_grad = np.repeat(cur_grad, repeat_time).reshape(self.A.data.shape)
 
         if self.A.requires_grad:
             if not isinstance(self.A.grad, np.ndarray):
-                #self.A.grad = cur_grad * C_grad
                 self.A.grad = cur_grad
             else:
-                #self.A.grad += cur_grad * C_grad
                 self.A.grad += cur_grad
 
 
@@ -425,7 +438,15 @@ class BatchElementWiseMul(Operation):
 
 
 class SetSubTensor(Operation):
-    # Set And Get SubTensor is a subset of Add Operation.
+    """
+    Set And Get SubTensor is a subset of Add Operation.
+
+    A = Set(coordinate_tuple)(A, B)
+
+    shape = get_shape_by_coord_tuple(coordinate_tuple)
+
+    assert shape == B.shape !!!
+    """
 
     def __init__(self, coordinate_tuple):
         super(SetSubTensor, self).__init__()
@@ -440,13 +461,19 @@ class SetSubTensor(Operation):
         self.A = args[0]
         self.B = args[1]
 
+        assert self.shape == self.B.data.shape
+
         C_data = self.A.data
         set_sub_ndarray(C_data, self.B.data, self.coordinate_tuple)
+
+        assert C_data.shape == self.A.data.shape
 
         C = Tensor(C_data)
 
         C.left_child = self.A
         C.right_child = self.B
+
+        self.output_shape = C_data.shape
 
         C.grad_fn = self
 
@@ -466,6 +493,9 @@ class SetSubTensor(Operation):
 
         # C_grad.shape == self.A.data.shape
 
+        if isinstance(C_grad, np.ndarray):
+            assert C_grad.shape == self.output_shape
+
         if self.A.requires_grad:
             assert isinstance(self.A.data, np.ndarray)
             if not isinstance(C_grad, np.ndarray):
@@ -482,6 +512,8 @@ class SetSubTensor(Operation):
                 self.A.grad += A_cur_grad
                 #set_sub_ndarray(self.A.grad, A_cur_grad, self.coordinate_tuple, is_add=True)
 
+            assert self.A.grad.shape == self.A.data.shape
+
         if self.B.requires_grad:
             assert isinstance(self.B.data, np.ndarray)
 
@@ -490,13 +522,15 @@ class SetSubTensor(Operation):
             else:
                 B_cur_grad = get_sub_ndarray(C_grad, self.coordinate_tuple)
 
+            assert B_cur_grad.shape == self.B.data.shape
+
             if not isinstance(self.B.grad, np.ndarray):
                 self.B.grad = np.zeros(self.B.data.shape)
                 self.B.grad = B_cur_grad
             else:
                 self.B.grad += B_cur_grad
 
-
+            assert self.B.grad.shape == self.B.data.shape
 
 
 class GetSubTensor(Operation):
@@ -609,3 +643,60 @@ class Reshape(Operation):
 
 
 
+class Repeat(Operation):
+    """
+    A = Repeat(A, target_shape)
+
+    """
+
+    def __init__(self, repeat_time=-1, target_shape=None):
+        super(Repeat, self).__init__()
+        self.repeat_time = repeat_time
+        self.target_shape = target_shape
+
+    def forward(self, *args):
+        assert len(args) == 1
+        assert isinstance(args[0], Tensor)
+        assert self.repeat_time > 0
+
+        self.A = args[0]
+
+        C_data = np.repeat(self.A.data, self.repeat_time)
+
+        if self.target_shape:
+            C_data = C_data.reshape(self.target_shape)
+
+        C = Tensor(C_data)
+        C.grad_fn = self
+        C.left_child = self.A
+        self.A.parent = C
+        self.output_shape = C_data.shape
+        if self.A.requires_grad:
+            C.requires_grad = True
+
+        return C
+
+    def backward(self, C_grad=None):
+        # The gradient of Operation `Repeat` is 1.
+
+        if isinstance(C_grad, np.ndarray):
+            assert C_grad.shape == self.output_shape
+
+        if self.A.requires_grad:
+            assert isinstance(self.A.data, np.ndarray)
+
+            if not isinstance(C_grad, np.ndarray):
+                A_cur_grad = np.ones(self.A.data.shape) # * cur_grad, cur_grad = ones
+            else:
+                A_cur_grad = C_grad.reshape(self.A.data.shape)
+
+            assert A_cur_grad.shape == self.A.data.shape
+
+            if not isinstance(self.A.grad, np.ndarray):
+                self.A.grad = np.zeros(self.A.data.shape)
+                self.A.grad = A_cur_grad
+            else:
+                assert self.A.grad.shape == self.A.data.shape
+                self.A.grad += A_cur_grad
+
+            assert self.A.grad.shape == self.A.data.shape

returing/returing/nn/operation/base/conv.py
@@ -2,7 +2,7 @@ from returing.nn.operation import Operation
 from returing.nn.tensor import Tensor
 
 from returing.nn.operation.base import Sum, BatchElementWiseMul, Add, \
-    GetSubTensor, SetSubTensor, Reshape
+    GetSubTensor, SetSubTensor, Reshape, Repeat
 from returing.nn.operation.base.pad import Padding2D
 from returing.nn.operation.base.slide import Sliding2D
 
@@ -19,15 +19,25 @@ class ConvCore2D(Operation):
         super(ConvCore2D, self).__init__()
         self.op_name = 'cross_correlation_2d'
         #self.name = name
-        self.kwargs = kwargs['kwargs']
+        #self.kwargs = kwargs['kwargs']
 
-        self.input_width = safe_read_dict(self.kwargs, 'input_width', 1)
-        self.input_height = safe_read_dict(self.kwargs, 'input_height', 1)
-        self.kernel_size = safe_read_dict(self.kwargs, 'kernel_size', 1)
-        self.stride = safe_read_dict(self.kwargs, 'stride', 1)
-        self.padding = safe_read_dict(self.kwargs, 'padding', 0)
+        """
+        self.input_width = safe_read_dict(self.kwargs, 'input_width', -1)
+        self.input_height = safe_read_dict(self.kwargs, 'input_height', -1)
+        self.kernel_size = safe_read_dict(self.kwargs, 'kernel_size', -1)
+        self.stride = safe_read_dict(self.kwargs, 'stride', -1)
+        self.padding = safe_read_dict(self.kwargs, 'padding', -1)
 
         self.W = safe_read_dict(self.kwargs, 'W', None) # weights, [K, K]
+        """
+
+        self.input_width = safe_read_dict(kwargs, 'input_width', -1)
+        self.input_height = safe_read_dict(kwargs, 'input_height', -1)
+        self.kernel_size = safe_read_dict(kwargs, 'kernel_size', -1)
+        self.stride = safe_read_dict(kwargs, 'stride', -1)
+        self.padding = safe_read_dict(kwargs, 'padding', -1)
+
+        self.W = safe_read_dict(kwargs, 'W', None)  # weights, [K, K]
 
 
     def set_weights(self, W):
@@ -77,6 +87,10 @@ class ConvCore2D(Operation):
         # padding_X: [n_samples, input_width+2P, input_weight+2P]
         padding_X = Padding2D(padding=self.padding)(X)
 
+        assert padding_X.data.shape == (n_samples,
+                                        self.input_width + 2 * self.padding,
+                                        self.input_height + 2 * self.padding)
+
         for i in range(output_width):
             for j in range(output_height):
 
@@ -87,22 +101,30 @@ class ConvCore2D(Operation):
                     stride=self.stride,
                     kernel_size=self.kernel_size)(padding_X)
 
+                assert sub_X.data.shape == (n_samples, self.kernel_size, self.kernel_size)
+
                 # sub_X: [n_samples, K, K]
                 # W: [K, K]
                 # Y_pred_ij: [n_samples, K, K]
                 # Rely on Right-align Broadcast of numpy in `ElementWiseMul`.
                 Y_pred_ij = BatchElementWiseMul()(sub_X, self.W)
 
-                # Y_pred_ij: [n_samples, 1], actually is `(n_samples, )` of np.sum().
-                # Must be reshaped before used !!!
-                Y_pred_ij = Sum(axis=(1, 2))(Y_pred_ij)
+                assert Y_pred_ij.data.shape == (n_samples, self.kernel_size, self.kernel_size)
+
+                # Y_pred_ij: [n_samples, 1, 1]
+                target_shape = (n_samples, 1, 1)
+                Y_pred_ij = Sum(axis=(1, 2), target_shape=target_shape)(Y_pred_ij)
+
+                assert Y_pred_ij.data.shape == (n_samples, 1, 1)
 
                 # Y_pred: [n_samples, output_width, output_height]
-                # Y_pred_ij: (n_samples, ) Reshape To [n_samples, 1]
+                # Y_pred_ij: [n_samples, 1, 1]
                 coord_tuple = ((0, n_samples),
                                (i, i+1),
                                (j, j+1))
                 Y_pred = SetSubTensor(coord_tuple)(Y_pred, Y_pred_ij)
+
+                assert Y_pred.data.shape == (n_samples, output_width, output_height)
 
         return Y_pred
 
@@ -112,15 +134,15 @@ class Conv2D(Operation):
         super(Conv2D, self).__init__()
         self.op_name = 'conv2d'
         self.name = None
-        self.kwargs = kwargs
+        #self.kwargs = kwargs
 
-        self.n_input_channel = safe_read_dict(kwargs, 'n_input_channel', 1)
-        self.input_width = safe_read_dict(kwargs, 'input_width', 1)
-        self.input_height = safe_read_dict(kwargs, 'input_height', 1)
-        self.n_output_channel = safe_read_dict(kwargs, 'n_output_channel', 1)
-        self.kernel_size = safe_read_dict(kwargs, 'kernel_size', 1)
-        self.stride = safe_read_dict(kwargs, 'stride', 1)
-        self.padding = safe_read_dict(kwargs, 'padding', 0)
+        self.n_input_channel = safe_read_dict(kwargs, 'n_input_channel', -1)
+        self.input_width = safe_read_dict(kwargs, 'input_width', -1)
+        self.input_height = safe_read_dict(kwargs, 'input_height', -1)
+        self.n_output_channel = safe_read_dict(kwargs, 'n_output_channel', -1)
+        self.kernel_size = safe_read_dict(kwargs, 'kernel_size', -1)
+        self.stride = safe_read_dict(kwargs, 'stride', -1)
+        self.padding = safe_read_dict(kwargs, 'padding', -1)
 
         self.initialization = safe_read_dict(kwargs, 'initialization', None)
         self.is_bias = safe_read_dict(kwargs, 'is_bias', True)
@@ -166,10 +188,8 @@ class Conv2D(Operation):
 
         # ===============================Important!!!================================
         # ===== Generated Process
-        Y = ListAdd()([Y_i]),
+        Y = SetSubTensor(i)([Y_i]),
             i = 0, 1, ..., n_output_channel-1,
-            ListAdd iterates over output channels,
-                (ListAdd is implemented by using Add iteratively)
 
         Y_i = ListAdd()([ A_j ]) + b_i
             j = 0, 1, ..., n_input_channel-1,
@@ -225,19 +245,35 @@ class Conv2D(Operation):
                                (0, self.kernel_size),
                                (0, self.kernel_size))
                 W_i = GetSubTensor(coord_tuple)(self.W)
+
+                # W_i: [K, K]
                 target_shape = (self.kernel_size, self.kernel_size)
                 W_i = Reshape(target_shape=target_shape)(W_i)
 
+                assert W_i.data.shape == (self.kernel_size, self.kernel_size)
+
                 # A_j: [n_samples, output_width, output_height]
                 # W_i: [K, K]
-                self.kwargs['W'] = W_i
-                A_j = ConvCore2D(kwargs=self.kwargs)(X_j)
-                #A_j = ConvCore2D()(X_j)
+                A_j = ConvCore2D(W=W_i,
+                                 input_width=self.input_width,
+                                 input_height=self.input_height,
+                                 kernel_size=self.kernel_size,
+                                 stride=self.stride,
+                                 padding=self.padding
+                                 )(X_j)
+
+                assert A_j.data.shape == (self.n_samples,
+                                          self.output_width,
+                                          self.output_height)
 
                 # Y_i: [n_samples, output_width, output_height]
                 # A_j: [n_samples, output_width, output_height]
                 Y_i = Add()(Y_i, A_j)
 
+            """
+            # Actually, bias can be added in the very end of the 
+            # output_chanel. 
+             
             if self.is_bias:
                 # b: [n_output_channel, 1]
                 # b_i: [1, 1]
@@ -250,13 +286,44 @@ class Conv2D(Operation):
                 # Rely on broadcast of numpy in `Add`.
                 # Here b_i is [1, 1], `Reshape` is not needed.
                 Y_i = Add()(Y_i, b_i)
+            """
 
-            # Y_i: [n_samples, output_width, output_height]
             # Y_pred = [n_sample, n_output_channel, output_width, output_height]
+            # Y_i: [n_samples, output_width, output_height]
             coord_tuple = ((0, self.n_samples),
                            (i, i+1),
                            (0, self.output_width),
                            (0, self.output_height))
+
+            target_shape = (self.n_samples, 1, self.output_width, self.output_height)
+            Y_i = Reshape(target_shape=target_shape)(Y_i)
+
+            assert Y_i.data.shape == target_shape
+
             Y_pred = SetSubTensor(coord_tuple)(Y_pred, Y_i)
+
+        if self.is_bias:
+            # Y_pred: [n_samples, n_output_channel, output_width, output_height]
+            # b: [n_output_channel, 1]
+
+            repeat_time = self.output_width * self.output_height
+            target_shape = (self.n_output_channel,
+                            self.output_width,
+                            self.output_height)
+
+            b = Repeat(repeat_time = repeat_time,
+                       target_shape = target_shape)(self.b)
+
+            assert b.data.shape == (self.n_output_channel,
+                                    self.output_width,
+                                    self.output_height)
+
+            # Note: Rely on Broadcast of numpy.
+            Y_pred = Add()(Y_pred, b)
+
+            assert Y_pred.data.shape == (self.n_samples,
+                                         self.n_output_channel,
+                                         self.output_width,
+                                         self.output_height)
 
         return Y_pred

returing/returing/nn/tensor/__init__.py
@@ -10,11 +10,13 @@ class Tensor(object):
     requires_grad = None
     grad_fn = None # Important, gradient_function is an Operation!
     grad = None
+    output_shape = None
 
     # Point to Tensor
     left_child = None
     right_child = None
     parent = None
+
 
     def __init__(self,
                  data=None,
@@ -36,7 +38,7 @@ class Tensor(object):
         if not self.grad_fn:
             return
 
-        grad_out = 1
+        grad_out = None
         if isinstance(self.grad, np.ndarray):
             grad_out = self.grad
 
@@ -48,9 +50,10 @@ class Tensor(object):
         if self.right_child:
             self.right_child.backward()
 
-    def print(self):
-        print(self.data)
+    def print(self, print_data=True):
 
+        if print_data:
+            print(self.data)
         if self.requires_grad:
             print('requires_grad =', self.requires_grad)
         if self.grad_fn: